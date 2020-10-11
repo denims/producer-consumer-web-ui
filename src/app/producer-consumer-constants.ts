1085175e2f30271d14b117918cef14c6c8a0a77d
Deni Simon <denimallel@gmail.com>
2020-10-11 23:49:43 +0530

    Made API URLS to relative ones

--- src/app/producer-consumer-constants.ts
+++ src/app/producer-consumer-constants.ts
@@ -1,8 +1,8 @@
 export class ProducerConsumerConstants {
-  public static TOPIC_CREATE_URL = 'http://localhost:8090/topic/create';
-  public static TOPIC_BASE_URL = 'http://localhost:8090/topic/';
+  public static TOPIC_CREATE_URL = './api/topic/create';
+  public static TOPIC_BASE_URL = './api/topic/';
 
-  public static PRODUCER_BASE_URL = 'http://localhost:8090/produce/';
+  public static PRODUCER_BASE_URL = './api/produce/';
 
-  public static CONSUMER_BASE_URL = 'http://localhost:8090/consumer/';
+  public static CONSUMER_BASE_URL = './api/consumer/';
 }
