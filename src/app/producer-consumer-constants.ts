acb14e722d7f06012276e6d5302081715b5f7553
Deni Simon <denimallel@gmail.com>
2020-10-11 23:27:35 +0530

    Corrected API port

--- src/app/producer-consumer-constants.ts
+++ src/app/producer-consumer-constants.ts
@@ -1,8 +1,8 @@
 export class ProducerConsumerConstants {
-  public static TOPIC_CREATE_URL = 'http://localhost:8080/topic/create';
-  public static TOPIC_BASE_URL = 'http://localhost:8080/topic/';
+  public static TOPIC_CREATE_URL = 'http://localhost:8090/topic/create';
+  public static TOPIC_BASE_URL = 'http://localhost:8090/topic/';
 
-  public static PRODUCER_BASE_URL = 'http://localhost:8080/produce/';
+  public static PRODUCER_BASE_URL = 'http://localhost:8090/produce/';
 
-  public static CONSUMER_BASE_URL = 'http://localhost:8080/consumer/';
+  public static CONSUMER_BASE_URL = 'http://localhost:8090/consumer/';
 }
